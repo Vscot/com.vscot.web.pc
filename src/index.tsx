dde76a3dcf582679cb555dfb42e30f6f2cb3a931
furyfu <furyfu@tencent.com>
2020-04-26 21:07:25 +0800

    chore: 新增redux依赖

--- src/index.tsx
+++ src/index.tsx
@@ -4,23 +4,23 @@ import globalContext, { initGlobalState } from "./stores";
 import { GlobalStoreState } from "./stores/interface";
 import globalReducer from "./stores/reducers";
 import BasicRouterLayout from "@/layouts/basicLayout";
-import zh_CN from 'antd/lib/locale-provider/zh_CN'
-import {ConfigProvider} from 'antd'
-import moment from 'moment'
-import 'moment/locale/zh-cn'
+import zh_CN from "antd/lib/locale-provider/zh_CN";
+import { ConfigProvider } from "antd";
+import moment from "moment";
+import "moment/locale/zh-cn";
 import "@/assets/css/index.less";
 
-moment.locale('zh_cn');
+moment.locale("zh_cn");
 
 const App = () => {
   const initState: GlobalStoreState = initGlobalState;
   const [state, dispatch] = useReducer(globalReducer, initState);
   return (
-      <ConfigProvider locale={zh_CN}>
-        <globalContext.Provider value={{ state, dispatch }}>
-          <BasicRouterLayout />
-        </globalContext.Provider>
-      </ConfigProvider>
+    <ConfigProvider locale={zh_CN}>
+      <globalContext.Provider value={{ state, dispatch }}>
+        <BasicRouterLayout />
+      </globalContext.Provider>
+    </ConfigProvider>
   );
 };
 
