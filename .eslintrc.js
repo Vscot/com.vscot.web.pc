144b9c034ed8d77061268179c26b20f4172ed823
furyfu <furyfu@tencent.com>
2020-07-12 09:49:38 +0800

    fix: eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
+const path = require('path');
+
 module.exports = {
   extends: [require.resolve('com.vscot.fabric/lib/tslint')],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
     REACT_APP_ENV: true,
+  },
+  // parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: path.join(__dirname, './tsconfig.json'),
   },
 };
